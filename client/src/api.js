0f0814d4e37155fc51e9e549b08a998daeb4f382
Siddhi Gaudani <siddhigaudani7@gmail.com>
2026-05-16 15:40:46 +0530

    fix: vercel routing issue

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "/api",
-//   withCredentials: true,
-//   headers: { "Content-Type": "application/json" },
-// });
+
 import axios from "axios";
 
 const api = axios.create({
